f3b49ecf0526ebd23791000a27f143684a18c72e
Glutenburg <126058947+Glutenburg@users.noreply.github.com>
2023-03-01 09:51:27 +0700

    Update AAA.lua

--- AAA.lua
+++ AAA.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TrapstarKSSKSKSKKS/Main/main/TrapHub.lua"))()
+_G.Key = "MRE0C-OBUYU-YKKZN"
+_G.DiscordId = "576484141772570656"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Natsuhanaki/Royx_PC/main/loader.lua"))();
